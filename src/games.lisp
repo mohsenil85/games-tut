7d7659da0a9d95de5aaf6668471fe9d0e85fc4c0
Logan Mohseni <mohsenil85@gmail.com>
2014-09-30 17:01:33 -0700

    working

--- src/games.lisp
+++ src/games.lisp
@@ -5,8 +5,9 @@
 (ql:quickload "parenscript")
 (defpackage games
   (:use :cl 
-       :cl-who 
+        :cl-who 
         :hunchentoot 
+        :postmodern
         :parenscript))
 (in-package :games)
 
@@ -17,34 +18,35 @@
    (votes :accessor votes
           :col-type integer
           :initform 0))
-  (:metaclass  postmodern:dao-class)
+  (:metaclass  dao-class)
   (:keys name))
 
 
-(postmodern:connect-toplevel "lmohseni" "lmohseni" "lmohseni" "localhost")
+(connect-toplevel "lmohseni" "lmohseni" "lmohseni" "localhost")
+(setf srv (make-instance 'hunchentoot:easy-acceptor :port 4242))
+(hunchentoot:start srv )
+(hunchentoot:stop srv)
+;(execute (dao-table-definition 'game ))
 (setf (html-mode) :html5)
 
- (defmethod vote-for (user-selected-game)
-   (let ((g (postmodern:get-dao 'game user-selected-game)))
-  (incf (votes g)) 
-  (postmodern:update-dao g) ))
+(defmethod vote-for (user-selected-game)
+  (let ((g (get-dao 'game user-selected-game)))
+    (incf (votes g)) 
+    (update-dao g) ))
 
 (defun game-from-name (name)
-  (postmodern:query (:select '* :from 'game :where (:like 'name name))) )
+  (query (:select '* :from 'game :where (:like 'name name))) )
 
 (defun game-stored? (game-name)
   (game-from-name game-name))
 
 
 (defun games ()
-(postmodern:query "select * from game order by votes desc"))
+  (query "select * from game order by votes desc"))
 
 (defun add-game (name)
   (unless (game-stored? name)
-    (postmodern:insert-dao (make-instance 'game :name name) )))
-
-
-
+    (insert-dao (make-instance 'game :name name) )))
 
 (defmacro standard-page ((&key title) &body body)
   `(with-html-output-to-string (*standard-output* nil :prologue t :indent t)
@@ -52,17 +54,14 @@
        (:head
          (:title ,title)
          (:link :type "text/css"
-                :rel "stylesheet"
-                :href "style.css"))
+          :rel "stylesheet"
+          :href "style.css"))
        (:body 
          (:div :id "header"
           (:h1 "this is the header"))
          ,@body))))
 
-(defvar srv (make-instance 'hunchentoot:easy-acceptor :port 4242))
-(hunchentoot:start srv )
-(hunchentoot:stop srv)
-      
+
 
 (defmacro define-url-fn ((name) &body body)
   `(progn
@@ -71,10 +70,8 @@
      (push (create-prefix-dispatcher ,(format nil "/~(~a~).htm" name ) ',name) *dispatch-table*))) 
 
 
-hunchentoot:*dispatch-table*
 (dolist ( game(games))
-  (format t "~a~% " (car game))
-  )
+  (format t "~a~%" (car game)))
 
 (with-html-output (dolist (game (games))
                     (:li 
@@ -85,23 +82,22 @@ hunchentoot:*dispatch-table*
 
 (define-url-fn (retro-games)
   (standard-page (:title "top retro games")
-                (:h1 "vote on your favorite")
-                (:p "missing a game?  add it " (:a :href "new-game.htm" "here"))
-                (:div :id "chart"
-                 (:ol
-                   (dolist (game (games))
-                     (htm
-                       (:li
-                         (:a :href (format nil "vote.htm?name=~a" (car game)) "VOTE!")
-                         (fmt "~A with ~d votes" (car game) (cdr game)))))))))
+    (:h1 "vote on your favorite")
+    (:p "missing a game?  add it " (:a :href "new-game.htm" "here"))
+    (:div :id "chart"
+     (:ol
+       (dolist (game (games))
+         (htm
+           (:li
+             (:a :href (format nil "vote.htm?name=~a" (car game)) "VOTE!")
+             (fmt "~A with ~d votes" (car game) (cdr game)))))))))
 
-(game-from-name "zap")
 (define-url-fn (vote)
-  (let ((game (game-from-name (parameter "name"))))
-    (if game
-      (vote-for (car game)))
+  (let ((game (parameter "name")))
+    (if (game-stored? game)
+      (vote-for game))
     (redirect "/retro-games.htm")))
-  
+
 
 (define-url-fn (new-game)
   (standard-page
@@ -110,11 +106,12 @@ hunchentoot:*dispatch-table*
     (:form :action "/game-added.htm" :method "post"
      (:p "game name" (:br)
       (:input :type "text"
-              :name "name"
-              :class "txt"))
-      (:p (:input :type "submit"
-              :value "add"
-              :class "btn")))))
+       :name "name"
+       :class "txt"))
+     (:p (:input :type "submit"
+          :value "add"
+          :class "btn")))))
+
 
 (define-url-fn (game-added)
   (let ((name (parameter "name")))
